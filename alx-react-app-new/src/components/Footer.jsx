e5235c5262bc7d1d0bc89953e365dbc797c7689d
mostafa refat <53091551+refatt@users.noreply.github.com>
2024-08-18 14:07:56 +0300

    Add files via upload

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer style={{color : "black" , fontWeight : 'bold' , background : 'grey'}}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+
+export default Footer;
