b542fc29540f17b264237b3f09aea2c2f3ff6021
mostafa refat <53091551+refatt@users.noreply.github.com>
2024-08-25 16:46:00 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,6 +4,14 @@ import create from 'zustand';
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  updateRecipe: (updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(recipe =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    )
+  })),
+  deleteRecipe: (recipeId) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== recipeId)
+  })),
   setRecipes: (recipes) => set({ recipes })
 }));
 
