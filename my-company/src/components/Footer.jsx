48f172e362596b4d3dd3634e346ba48619b5ccae
mostafa refat <53091551+refatt@users.noreply.github.com>
2024-08-26 13:38:44 +0300

    Create Footer.jsx

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', backgroundColor: 'navy', color: 'white', textAlign: 'center', position: 'fixed', width: '100%', bottom: '0' }}>
+      <p>&copy; 2024 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
