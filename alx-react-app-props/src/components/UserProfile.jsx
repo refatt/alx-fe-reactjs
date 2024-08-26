7c71fb737800e3706fbc34c77db39228c3c78106
mostafa refat <53091551+refatt@users.noreply.github.com>
2024-08-26 13:22:19 +0300

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
+import React, { useContext } from 'react';
+import { UserContext } from '../UserContext';
 
-  export default UserProfile;
+function UserProfile() {
+  const { name, age, bio } = useContext(UserContext);
+
+  return (
+    <div style={{ border: '1px solid gray', padding: '20px', margin: '20px', borderRadius: '5px', backgroundColor: '#f9f9f9' }}>
+      <h2 style={{ color: 'blue', fontSize: '24px' }}>{name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold', fontSize: '18px' }}>{age}</span></p>
+      <p style={{ fontSize: '16px', color: '#555' }}>Bio: {bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
