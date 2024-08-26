b3c9d43c162604072d1b8ac945fe22a5405e69ab
mostafa refat <53091551+refatt@users.noreply.github.com>
2024-08-26 13:50:05 +0300

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for routing
-import useRecipeStore from '../recipeStore';
+import { Link } from 'react-router-dom';
+import useRecipeStore from '../store/recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.filteredRecipes);
+  const recipes = useRecipeStore(state => state.filteredRecipes.length ? state.filteredRecipes : state.recipes);
 
   return (
     <div>
       {recipes.map(recipe => (
         <div key={recipe.id}>
-          <h3>
-            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-          </h3>
-          <p>{recipe.description}</p>
+          <Link to={`/recipe/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </Link>
         </div>
       ))}
     </div>
