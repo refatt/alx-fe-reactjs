9d45ab0764eccc71b56419babe0aca84f1f10c2e
mostafa refat <53091551+refatt@users.noreply.github.com>
2024-09-01 18:42:13 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,11 +1,16 @@
-// src/App.jsx
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Login from './components/Login';
 import BlogPost from './components/BlogPost';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   const isAuthenticated = true; // Simulate an authentication check
+
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile/*" element={
@@ -14,8 +19,10 @@ function App() {
           </ProtectedRoute>
         } />
         <Route path="/login" element={<Login />} />
-        <Route path="/post/:id" element={<BlogPost />} />
+        <Route path="/post/:id" element={<BlogPost />} /> {/* Dynamic Route */}
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
+
+export default App;
