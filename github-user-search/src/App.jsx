e954e075a0fa176550f4b81ed345c0054ef2a65e
mostafa refat <53091551+refatt@users.noreply.github.com>
2024-09-28 03:10:44 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,28 +3,28 @@ import Search from './components/Search';
 import { fetchUserData } from './services/githubService';
 
 function App() {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const handleSearch = async (username) => {
-    setLoading(true);   // Start loading state
-    setError('');       // Clear any previous errors
-    setUserData(null);  // Clear previous user data
+  const handleSearch = async ({ username, location, minRepos }) => {
+    setLoading(true);
+    setError('');
+    setUserData([]);
 
     try {
-      const data = await fetchUserData(username);
-      setUserData(data); // Set the user data received from the API
+      const data = await fetchUserData({ username, location, minRepos });
+      setUserData(data);
     } catch (err) {
-      setError('Looks like we can’t find the user.');  // Set error message
+      setError('Looks like we can’t find any users matching your criteria.');
     } finally {
-      setLoading(false);  // Stop loading state
+      setLoading(false);
     }
   };
 
   return (
     <div className="App">
-      <h1>GitHub User Search</h1>
+      <h1 className="text-center text-2xl font-bold">GitHub User Search</h1>
       <Search 
         onSearch={handleSearch} 
         loading={loading} 
