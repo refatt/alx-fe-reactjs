f3b411503436da4a59fb4bdae18fa19f494a359d
mostafa refat <53091551+refatt@users.noreply.github.com>
2024-09-28 03:00:12 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.github.com/users';
-
 export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${API_URL}/${username}`);
-    return response.data;
-  } catch (error) {
-    throw new Error('User not found');
-  }
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;  // Return the user data from the API response
 };
